2da809cab6e4b57268f84e546ee584e5bf7f493a
DZH777 <DZH777@list.ru>
2021-02-12 20:15:08 +0300

    Chapter 1 scripts

---
+++ scripts/hello_world.py
@@ -0,0 +1,17 @@
+print("Hello Python world!")
+message = "hello python world!!!"
+print(message.title())
+first_name = "ada"
+last_name = "lovelace"
+full_name = f"{first_name} {last_name}"
+print(full_name.upper())
+print(f"hi, {full_name.upper()}")
+print(f"\thi,\n{full_name.upper()}")
+favorite_language = ' python '
+print(favorite_language.strip())
+print(0.2 + 0.1)
+print(0.2 + 0.2)
+universe_age = 14_000_000_000
+print(universe_age)
+# Constant
+MAX_CONNECTIONS = 5000
